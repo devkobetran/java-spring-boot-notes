eb8351525a29afc17a5347f1c3e1b6de86f8b043
kotran222 <kobetrankhoi222@gmail.com>
2024-08-22 23:24:01 -0500

    Deploy website - based on b982691fefffc67b3dd69ea2de617c1e95c7bb66

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/kobetran/Documents/Personal_Projects/WebDevelopment/java-spring-boot-notes/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/kobetran/Documents/Personal_Projects/WebDevelopment/java-spring-boot-notes/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/kobetran/Documents/Personal_Projects/WebDevelopment/java-spring-boot-notes/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/kobetran/Documents/Personal_Projects/WebDevelopment/java-spring-boot-notes/src/css/custom.css"),
-];
